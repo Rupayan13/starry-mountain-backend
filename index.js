618889fef0cf70d538d51f2fd1627f4f3687bb61
Rupayan13 <send2rupayan2002@gmail.com>
2025-10-26 22:42:16 +0530

    updateFeedbacks of id route has been added

--- index.js
+++ index.js
@@ -173,13 +173,32 @@ app.get("/getFeedbacks", async (req, res) => {
     }
 });
 
-app.get("/getFeedbacks/:id", async (req, res) => {
+app.get("/getFeedback/:id", async (req, res) => {
     const id = req.params.id;
     try {
         const feedback = await Feedback.findById(id);
         res.status(200).json(feedback);
     } catch (err) {
         console.error("Error fetching feedback:", err);
+        res.status(500).json(err);
+    }
+});
+
+app.put("/updateFeedback/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const feedback = await Feedback.findById(id);
+        if (!feedback) {
+            return res.status(404).json({ error: "Feedback not found" });
+        }
+        const updatedFeedback = await Feedback.findByIdAndUpdate({ _id: id },
+            {
+                name: req.body.name,
+                email: req.body.email,
+                feedback: req.body.feedback
+            });
+        res.json(updatedFeedback);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
